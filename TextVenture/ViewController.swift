a82e1317e284b0ad52124d0d28b7ff93952117a4
Grant Meagher <grantme@gher.com>
2016-03-08 20:21:12 -0600

    Initial Commit

---
+++ TextVenture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextVenture
+//
+//  Created by Grant Meagher on 3/8/16.
+//  Copyright © 2016 Grant Meagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
